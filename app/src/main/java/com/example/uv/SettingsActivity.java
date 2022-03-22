8d877bec88ae34eae586931f0d850d844432b21f
Judah Ben-Eliezer <judahbeneliezer@gmail.com>
2022-03-22 01:36:18 -0400

    ...

--- app/src/main/java/com/example/uv/SettingsActivity.java
+++ app/src/main/java/com/example/uv/SettingsActivity.java
@@ -1,12 +1,12 @@
 package com.example.uv;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 
 //import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-//import androidx.preference.PreferenceFragmentCompat;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -14,31 +14,12 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
-//        if (savedInstanceState == null) {
-//            getSupportFragmentManager()
-//                    .beginTransaction()
-//                    .replace(R.id.title, new SettingsFragment())
-//                    .commit();
-//        }
-//        ActionBar actionBar = getSupportActionBar();
-//        if (actionBar != null) {
-//            actionBar.setDisplayHomeAsUpEnabled(true);
-//        }
 
         backButton = findViewById(R.id.backButton);
         backButton.setOnClickListener(view -> finish());
+        Log.e("SettingsActivite", "started settings activity.");
     }
-//
-//    public static class SettingsFragment extends PreferenceFragmentCompat {
-//        @Override
-//        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-//            setPreferencesFromResource(R.xml.root_preferences, rootKey);
-//        }
-//    }
-
-//    private displaySkinTypeDescription(String skin_type) {
-//
-//    }
-
     private ImageButton backButton;
+
+
 }
